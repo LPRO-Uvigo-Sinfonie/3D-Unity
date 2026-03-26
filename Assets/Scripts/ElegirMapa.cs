9ef5fb844759279f703f6ffb6b46fcfa9d2a4f12
Alba Vazquez <runo-mola12@hotmail.es>
2026-03-26 23:35:03 +0100

    Agregado menu principal
    Inicio de un menú principal en donde se podrá elegir mapa, instrumentos y partitura (midi)

---
+++ Assets/Scripts/ElegirMapa.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class CElegirMapa : MonoBehaviour
+{
+    public void IrAlMapa()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
